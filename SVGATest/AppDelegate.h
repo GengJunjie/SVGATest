79d5e39fd47c2b46e89b649d17bbd5b5abc97650
gengjunjie <gengjunjie@lespark.us>
2018-02-28 14:29:04 +0800

    Initial Commit

---
+++ SVGATest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SVGATest
+//
+//  Created by gengjunjie on 2018/2/28.
+//  Copyright © 2018年 gengjunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
